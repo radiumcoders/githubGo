e29c09e79d558864bd752d6a1ce46bda0356bd3f
radiumcoders <radiumcoders@gmail.com>
2025-10-09 20:19:30 +0530

    calculator.go file base for project on day 10

---
+++ funcs/calculator.go
@@ -0,0 +1,37 @@
+package funcs
+
+import "errors"
+
+// its recommended to use a struct for error handling
+type ReturnValue struct {
+	Result float32
+	Error  error
+}
+
+func Multiplication(a, b float32) ReturnValue {
+	if a == 0 || b == 0 {
+		return ReturnValue{0, errors.New("Multiplication by 0 — who wants to do that?!")}
+	}
+	return ReturnValue{a * b, nil}
+}
+
+func Division(a, b float32) ReturnValue {
+	if b == 0 {
+		return ReturnValue{0, errors.New("Division by 0 — who wants to do that?!")}
+	}
+	return ReturnValue{a / b, nil}
+}
+
+func Addition(a, b float32) ReturnValue {
+	if a == 0 || b == 0 {
+		return ReturnValue{0, errors.New("Addition by 0 — who wants to do that?!")}
+	}
+	return ReturnValue{a + b, nil}
+}
+
+func Subtraction(a, b float32) ReturnValue{
+	if a == 0 || b == 0 {
+		return ReturnValue{0, errors.New("Subtraction by 0 — who wants to do that?!")}
+	}
+	return ReturnValue{a - b, nil}
+}
--- funcs/example.go
+++ funcs/example.go
@@ -1,22 +1,7 @@
 package funcs
 
-import "errors"
-// its recommended to use a struct for error handling
-type ReturnValue struct {
-	Result int
-	Error  error
-}
+import "fmt"
 
-func Multiplication(a, b int) ReturnValue {
-	if a == 0 || b == 0 {
-		return ReturnValue{0, errors.New("Multiplication by 0 — who wants to do that?!")}
-	}
-	return ReturnValue{a * b, nil}
-}
-
-func Division(a, b int) ReturnValue {
-	if b == 0 {
-		return ReturnValue{0, errors.New("Division by 0 — who wants to do that?!")}
-	}
-	return ReturnValue{a / b, nil}
+func PrintHelloWorld() {
+	fmt.Println("Hello, World!")
 }
