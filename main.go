4bc00f2adf7c47ceab285c7d934f6aceffb16d5b
radiumcoders <radiumcoders@gmail.com>
2025-10-09 22:32:25 +0530

    readme update :D

--- main.go
+++ main.go
@@ -2,21 +2,28 @@ package main
 
 import (
 	"fmt"
-	"github.com/radiumcoders/githubGo/funcs"
+	"strconv"
+	// "github.com/radiumcoders/githubGo/funcs"
 )
 
 func main() {
-	var a float32 = 10.0
-	var b float32 = 20.0
-	rv := funcs.Multiplication(&a, &b)
-	if rv.Error != nil {
-		fmt.Println("Error:", rv.Error)
-	} else {
-		fmt.Println("Result:", rv.Result)
+	// var a float32 = 10.0
+	// var b float32 = 0
+	// rv := funcs.Multiplication(&a, &b)
+	// if rv.Error != nil {
+	// 	fmt.Println("Error:", rv.Error)
+	// } else {
+	// 	fmt.Println("Result:", rv.Result)
+	// }
+	a := 12
+	fmt.Println(a)
+	// converting int to string don't give error
+	b := strconv.Itoa(a)
+	fmt.Println("Result:", b)
+	// converting string to int
+	c , err := strconv.Atoi(b)
+	if err != nil {
+		fmt.Println("Error:", err)
 	}
-	// num := 10
-	// ptr := &num
-	// fmt.Println(&ptr)
-	// fmt.Println(*ptr)
-	// fmt.Println(ptr)
+	fmt.Println(c)
 }
