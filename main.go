1b909bbab155b83ee348d7f0dc7f12bf829faf79
radiumcoders <radiumcoders@gmail.com>
2025-10-09 21:16:40 +0530

    pointers for Day 5

--- main.go
+++ main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"fmt"
 
-	"github.com/radiumcoders/githubGo/funcs"
+	// "github.com/radiumcoders/githubGo/funcs"
 )
 
 func main() {
-	rv := funcs.Multiplication(12, 0)
-	if rv.Error != nil {
-		fmt.Println("Error:", rv.Error)
-	} else {
-		fmt.Println("Result:", rv.Result)
-	}
+	// rv := funcs.Multiplication(12, 0)
+	// if rv.Error != nil {
+	// 	fmt.Println("Error:", rv.Error)
+	// } else {
+	// 	fmt.Println("Result:", rv.Result)
+	// }
+	num := 10 
+	ptr := &num
+	fmt.Println("Pointer:", ptr)
+	fmt.Println("Value:", *ptr)
+	fmt.Println(&ptr)
 }
